cb0ee2138080275b22555365386a47ed7ecc7c4a
gmc <nabacg@gmail.com>
2010-10-10 23:52:34 +0100

    zmiany na repozytorium glownym, podejscie drugie ;)

--- src/PubsProject/pub4me/views.py
+++ src/PubsProject/pub4me/views.py
@@ -11,8 +11,6 @@ def index(request):
     return render_to_response('pub4me/index.html', context_instance=RequestContext(request))
 
 def pub_autocomplete(request):
-    
-    # p = get_object_or_404(Poll, pk=poll_id)
     if request.method == 'GET':
         if request.GET.__contains__('term'):
             term = request.GET.__getitem__('term')
@@ -20,11 +18,5 @@ def pub_autocomplete(request):
             query_result = Pub.objects.filter(name__icontains=term)[:10]
             for obj in query_result:
                 suggestions.append({"id": obj.pk, "label": obj.name + " - " + obj.location, "value": obj.name + " - " + obj.location})
-            #result = [ {x.name , x.location} for x in query_result ]
-            #json_data = serializers.serialize('json', suggestions)
             json_data = simplejson.dumps(suggestions)
             return HttpResponse(json_data,'application/javascript')
-
-
-    return render_to_response('pub4me/pub_autocomplete_json.html', {'term': 'chuja-zle wywoalenie'}, context_instance=RequestContext(request))
-    
