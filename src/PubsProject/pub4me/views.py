2cbff542d0a2cde3e4fc12a6392e8cb6d3f24cd6
gmc <nabacg@gmail.com>
2010-10-11 08:07:12 +0100

    zwracanie bledu

--- src/PubsProject/pub4me/views.py
+++ src/PubsProject/pub4me/views.py
@@ -14,9 +14,14 @@ def pub_autocomplete(request):
     if request.method == 'GET':
         if request.GET.__contains__('term'):
             term = request.GET.__getitem__('term')
-            suggestions = []
+#            suggestions = []
             query_result = Pub.objects.filter(name__icontains=term)[:10]
-            for obj in query_result:
-                suggestions.append({"id": obj.pk, "label": obj.name + " - " + obj.location, "value": obj.name + " - " + obj.location})
-            json_data = simplejson.dumps(suggestions)
+#            for obj in query_result:
+#                suggestions.append({"id": obj.pk, "label": obj.name + " - " + obj.location, "value": obj.name + " - " + obj.location})
+            json_data = simplejson.dumps(map(lambda r : {
+                    "id": r.pk,
+                    "label": "%s - %s" % (r.name, r.location),
+                    "value": "%s - %s"%(r.name, r.location)}),
+                query_result)
             return HttpResponse(json_data,'application/javascript')
+    return render_to_response('pub4me/pub_autocomplete_json.html', {'term': 'Error'}, context_instance=RequestContext(request))
