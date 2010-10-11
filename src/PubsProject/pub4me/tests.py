c2a8015b50e2bfed09906b220c7f882f2081f508
gmc <nabacg@gmail.com>
2010-10-11 21:25:44 +0100

    Testy do widokow index i pub_autocomplete zapewnienie, ze views.pub_autocomplete zawsze zwraca HttpResponse

--- src/PubsProject/pub4me/tests.py
+++ src/PubsProject/pub4me/tests.py
@@ -4,20 +4,58 @@ unittest). These will both pass when you run "manage.py test".
 
 Replace these with more appropriate tests for your application.
 """
-
+from django.test.client import Client
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+from pub4me.models import Pub, City
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+TEST_TERM = "Karlik"
+TEST_PUB_NAME = TEST_TERM + "Pub"
+TEST_CITY_ID = "1"
+TEST_PL_NAME = "Dublin"
+TEST_EN_NAME = TEST_PL_NAME
+TEST_EXT_SERVICE_ID = 666
+TEST_ERR_MSG_NAME = "err_msg"
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+def load_test_data(city_id, pl_name, en_name, pub_name, ext_id):
+    test_city = City.objects.create(id = city_id, pl_name= pl_name, en_name=en_name)
+    test_pub = Pub(name = pub_name, city = test_city, ext_service_id_kk = ext_id)
+    test_pub.save()
 
->>> 1 + 1 == 2
-True
-"""}
+class JsViewTest(TestCase):
+
+
+    def setUp(self):
+        self._c = Client()
+        load_test_data(TEST_CITY_ID, TEST_PL_NAME, TEST_EN_NAME, TEST_PUB_NAME, TEST_EXT_SERVICE_ID)
+
+    """Testuje glowny widok zwracajacy Landing Page"""
+    def test_index_view(self):
+        response = self._c.post(reverse("PubsProject.pub4me.views.index"))
+        self.assertNotEqual(None, response)
+        self.assertEqual(200, response.status_code, "Index view failed to respond")
+
+
+    def test_valid_pub_search(self):
+        response = self._c.get(reverse("PubsProject.pub4me.views.pub_autocomplete"), {"term": TEST_PUB_NAME})
+        self.assertNotEqual(None, response)
+        self.assertEqual(200, response.status_code, "Autocomplete view failed to respond")
+        self.assertNotEqual(0, len(response.content))
+        response_obj = eval(response.content)
+        self.assertNotEqual(0, len(response_obj))
+
+    def test_invalid_pub_search(self):
+        response = self._c.post(reverse("PubsProject.pub4me.views.pub_autocomplete"))
+        self.assertEqual(200, response.status_code, "Faktycznie aplikacja sie wywalila")
+        response_obj = eval(response.content)
+        err_msg = response_obj[TEST_ERR_MSG_NAME]
+        self.assertNotEqual(None, err_msg)
+        self.assertNotEqual(0, len(err_msg))
+
+
+
+
+    
+
+
 
--- src/PubsProject/pub4me/views.py
+++ src/PubsProject/pub4me/views.py
@@ -1,7 +1,6 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
-#from django.core import serializers
 from django.utils import simplejson
 from PubsProject.pub4me.models import Pub
 
@@ -14,14 +13,13 @@ def pub_autocomplete(request):
     if request.method == 'GET':
         if request.GET.__contains__('term'):
             term = request.GET.__getitem__('term')
-#            suggestions = []
             query_result = Pub.objects.filter(name__icontains=term)[:10]
-#            for obj in query_result:
-#                suggestions.append({"id": obj.pk, "label": obj.name + " - " + obj.location, "value": obj.name + " - " + obj.location})
+            
             json_data = simplejson.dumps(map(lambda r : {
                     "id": r.pk,
                     "label": "%s - %s" % (r.name, r.location),
-                    "value": "%s - %s"%(r.name, r.location)}),
-                query_result)
+                    "value": "%s - %s" % (r.name, r.location)},
+                query_result))
             return HttpResponse(json_data,'application/javascript')
-    return render_to_response('pub4me/pub_autocomplete_json.html', {'term': 'Error'}, context_instance=RequestContext(request))
+    err_msg = "Unable to find Pub 4 you.."
+    return HttpResponse(simplejson.dumps({"err_msg": err_msg}))
