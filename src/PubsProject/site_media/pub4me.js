b52eb44ae84f038cab33ec0cbf7e38dfd8af7d16
pawelsmietanka <pawelsmietanka@gmail.com>
2010-10-23 22:32:18 +0200

    Wywalenie starej funkcji js dodającej sklonowane pole formularza

--- src/PubsProject/site_media/pub4me.js
+++ src/PubsProject/site_media/pub4me.js
@@ -6,49 +6,10 @@ $(function() {
 			var nameInputId = $(this).attr("id");
 			var idInputId = nameInputId.replace("name", "id");
 			$("#"+idInputId).val(ui.item.id);
-			$(".more").show();			
         }
     });
 });
 
 $(document).ready(function(){
-		
-	var $pubInput;
-	
-	var $last = $("input.autocomplete").last();
-	        
-    $(".more").click(function(){
-    	$pubInput = $("input.autocomplete").eq(0).clone();
-    	$(".primary").before($pubInput);   	
-    	// $(".more").hide();
-    	$("input.autocomplete").autocomplete({
-            source: "pub_autocomplete",
-    		minLength: 2,
-            select: function(event, ui){  
-        	//  $(".more").show();
-            }
-        });
-    });
-    
-/*    
-    $("#add_place_submit").click(function(){
-        $(this).hide();
-        $("#second_place_name").show();
-        return false;
-    });
-    
-    $("#second_place_name_submit").click(function(){
-        $(this).hide();
-        $("#second_place_name").hide();
-        $("#result").show();
-        return false;
-    });
-    
-    $("#next").add("#been").click(function(){
-        $("#result").hide('slow').find("#result_place").text('Betel');
-        $("#result").show('slow');
-        return false;
-    });
-*/
-    
+		        
 });
