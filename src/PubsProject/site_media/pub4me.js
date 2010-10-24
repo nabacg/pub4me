36e6edee7895a28c081446e90e0523d7d387332a
pawelsmietanka <pawelsmietanka@gmail.com>
2010-10-24 15:41:00 +0200

    autouzupełnianie dla nowo dodanych pól

--- src/PubsProject/site_media/pub4me.js
+++ src/PubsProject/site_media/pub4me.js
@@ -1,5 +1,24 @@
 $(function() {
-	$("input.autocomplete").autocomplete({
+	
+	$('form.pubs div').formset({
+		addText: '+ dodaj kolejną',
+		addCssClass: 'addformlink',
+		deleteCssClass: 'removeformlink',
+		added: function (row) {
+		row.find(".autocomplete").autocomplete({
+	        source: "pub_autocomplete",
+			minLength: 2,
+	        select: function(event, ui){
+				var nameInputId = $(this).attr("id");
+				var idInputId = nameInputId.replace("name", "id");
+				$("#"+idInputId).val(ui.item.id);
+	        }
+	    	});	
+			row.find(".autocomplete").autocomplete("enable");
+		}
+    });
+	
+	$(".autocomplete").autocomplete({
         source: "pub_autocomplete",
 		minLength: 2,
         select: function(event, ui){
@@ -8,8 +27,4 @@ $(function() {
 			$("#"+idInputId).val(ui.item.id);
         }
     });
-});
-
-$(document).ready(function(){
-		        
 });
