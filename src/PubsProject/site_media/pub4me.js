b14886b4a0e6979e86e21084938bcc4aa1498a4f
pawelsmietanka <pawelsmietanka@gmail.com>
2010-12-18 18:56:06 +0100

    wyświetlanie i chowanie informacji o dodawanym miejcu

--- src/PubsProject/site_media/pub4me.js
+++ src/PubsProject/site_media/pub4me.js
@@ -2,45 +2,37 @@ $(function() {
 	
 	var $div;
 	
+	jQuery.fn.myautocomplete = function() {
+	    $(this).autocomplete({
+			source: function( request, response ) {
+				$.getJSON( "pub_autocomplete", request, function(data){					
+					response(data);
+					if (data.length == 0) $div.find('.notify').show();
+					if (data.length > 0) $div.find('.notify').hide();
+				});
+			},
+			minLength: 2,
+			search: function(event, ui) {			
+				$div = $(this).parent();
+			},
+		    select: function(event, ui){
+				var nameInputId = $(this).attr("id");
+				var idInputId = nameInputId.replace("name", "id");
+				$("#"+idInputId).val(ui.item.id);
+		    }
+		});
+	};
+	
+	$(".autocomplete").myautocomplete();	
+	
 	$('form.pubs div').formset({
 		addText: '+ dodaj kolejną',
 		addCssClass: 'addformlink',
 		deleteCssClass: 'removeformlink',
 		added: function (row) {
-		row.find(".autocomplete").autocomplete({
-	        source: "pub_autocomplete",
-			minLength: 2,
-	        select: function(event, ui){
-				var nameInputId = $(this).attr("id");
-				var idInputId = nameInputId.replace("name", "id");
-				$("#"+idInputId).val(ui.item.id);
-	        }
-	    	});	
-			row.find(".autocomplete").autocomplete("enable");
+		row.find(".autocomplete").myautocomplete();
+		row.find(".autocomplete").autocomplete("enable");
 		}
-    });
-	
-	$(".autocomplete").autocomplete({
-		source: function( request, response ) {
-			$.getJSON( "pub_autocomplete", 
-				request, 
-				function(data){
-					response(data);
-					if (data.length == 0) {
-						$('.notify').show();
-					}
-				}
-			);
-		},
-		minLength: 2,
-		search: function(event, ui) {
-			$div = $(this);
-		},
-        select: function(event, ui){
-			var nameInputId = $(this).attr("id");
-			var idInputId = nameInputId.replace("name", "id");
-			$("#"+idInputId).val(ui.item.id);
-        }
     });
 	
 	$(".primary").click(function(){
